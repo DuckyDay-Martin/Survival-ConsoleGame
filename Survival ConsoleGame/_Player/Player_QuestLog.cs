f1104bf0ab99c8c43a05cf644b752fb0bed95f1c
Мартин Кондакчиев <martinkondakciev888@gmail.com>
2024-06-23 02:30:26 +0300

    .

--- Survival ConsoleGame/_Player/Player_QuestLog.cs
+++ Survival ConsoleGame/_Player/Player_QuestLog.cs
@@ -20,9 +20,29 @@ namespace Survival_ConsoleGame
         }
         public void Player_QuestLogView(PlayerInventory inventory)
         {
-            Console.WriteLine("~Quest Log~");          
-            Console.WriteLine("1.Gather 15 Wood(+10 Gold)");
-            Console.WriteLine("2.Gather 10 Rocks(+15 Gold)");
+            //The quest will be updateing when is acomplished 
+            //So we can keep it simple on the screen constantly there will be 3 Quests at the same time           
+            Console.WriteLine("~Quest Log~");
+            if (inventory.CountItems(1) < 15)
+            {
+                Console.WriteLine("1.Gather 15 Wood(+10 Gold)");
+            }
+            else 
+            {
+                //Add Gold
+                //Change the quest
+            }
+
+            if (inventory.CountItems(2) < 10)
+            {
+                Console.WriteLine("2.Gather 10 Rocks(+15 Gold)");
+            }
+            else 
+            {
+                //Add Gold
+                //Change the quest
+            }
+
             Console.WriteLine("3.Gather 5 Meat(+6 Gold)");
 
             
